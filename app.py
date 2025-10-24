e0e07b7d8e77bbe67bd8b08e3453fc4d503bebb1
Maxs67120 <max.sehmisch@live.fr>
2025-10-24 14:54:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 from PIL import Image
 import os
 
-# Load and display logo
-logo = Image.open("logo.png")  # Assure-toi que le fichier existe
-st.image(logo, width=150)
-
 # List of accepted vector file extensions
 vector_extensions = ['.ai', '.svg', '.eps']
 
